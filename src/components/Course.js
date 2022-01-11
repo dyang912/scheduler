418fb855d0a7f15a61f47da6fe85ed35628be586
Dong Yang <outskidding@gmail.com>
2022-01-11 16:35:16 -0600

    Write to Firebase

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,10 +1,31 @@
 import {hasConflict} from "../utilities/times";
 import {getCourseNumber, getCourseTerm} from "../utilities/utilities";
+import {timeParts} from "../utilities/times";
+import {setData} from "../utilities/firebase";
+import useDoubleClick from 'use-double-click';
 import React from "react";
 
 const toggle = (x, lst) => (
     lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
+
+const getCourseMeetingData = course => {
+    const meets = prompt('Enter meeting data: MTuWThF hh:mm-hh:mm', course.meets);
+    const valid = !meets || timeParts(meets).days;
+    if (valid) return meets;
+    alert('Invalid meeting data');
+    return null;
+};
+
+const reschedule = async (course, meets) => {
+    if (meets && window.confirm(`Change ${course.id} to ${meets}?`)) {
+        try {
+            await setData(`/courses/${course.id}/meets`, meets);
+        } catch (error) {
+            alert(error);
+        }
+    }
+};
 
 export const Course = ({ course, selected, setSelected }) => {
     const isSelected = selected.includes(course);
@@ -14,7 +35,10 @@ export const Course = ({ course, selected, setSelected }) => {
     };
 
     return (
-        <div className="card m-1 p-1" style={style} onClick={isDisabled ? null : () => setSelected(toggle(course, selected))} >
+        <div className="card m-1 p-1" style={style}
+             onClick={isDisabled ? null : () => setSelected(toggle(course, selected))}
+             onDoubleClick={ () => reschedule(course, getCourseMeetingData(course)) }
+        >
             <div className="card-body">
                 <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
                 <div className="card-text">{ course.title }</div>
